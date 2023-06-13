3fab0a890345310821abe49e5711cdd1dce2500f
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-06-13 07:18:24 +0530

    Update Dashboard.js
    adding comments and checking indentation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,55 +5,43 @@ import {LoginContext} from './ContextProvider/Context';
 import {BACKEND_URL} from "../staticData.js"
 
 const Dashboard = () =>{
-
 	const{loginData, setLoginData} = useContext(LoginContext);
-
 	const navigate = useNavigate();
-
-   const DashboardValid = async () =>{
-
-   	//validating the user
-
-   	let token = localStorage.getItem("access-token");
-   	if(!token)
+	//validating the user before displaying the dashboard
+        const DashboardValid = async () =>{
+   	   //validating the user
+   	   let token = localStorage.getItem("access-token");
+   	   if(!token)
    		navigate("*")
-   
-    const res = await axios.get(`${BACKEND_URL}/user/validateUser`,
-    	{
-                headers: 
+           const res = await axios.get(`${BACKEND_URL}/user/validateUser`,
+            {
+                 headers: 
                 {
                     "access-token": token
                 }
-        }
-    	);
+             });
      
-
-    if(res.status == 400 || !res.data)
-    {
-    	navigate("*")
-    }
-    else
-    {
-    	console.log("user verified");
-    	setLoginData(res.data);
-    }
-
-
+          if(res.status == 400 || !res.data)
+             {
+    	      navigate("*")
+             }
+          else
+            {
+          	console.log("user verified");
+        	setLoginData(res.data);
+            }
    }
 
    useEffect(()=>{
-
    	DashboardValid();
-
    }, [])
-
 
 	return(
 		<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-		<img src="./man.png" alt ="man icon" style={{ width: "200px", marginTop: 20 }}/>
-         <h1>User Email:{loginData? loginData.email: ""}</h1>
+		  <img src="./man.png" alt ="man icon" style={{ width: "200px", marginTop: 20 }}/>
+                  <h1>User Email:{loginData? loginData.email: ""}</h1>
 		</div>
 	);
-}
+  }
 
 export default Dashboard;
