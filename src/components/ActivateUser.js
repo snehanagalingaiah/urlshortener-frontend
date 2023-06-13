c7bd9b2d66cb999693224aa5c38faf0761c7831e
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-06-13 06:59:46 +0530

    Update ActivateUser.js
    adding comments, checking indentation

--- src/components/ActivateUser.js
+++ src/components/ActivateUser.js
@@ -4,54 +4,41 @@ import { useParams,useNavigate} from "react-router-dom";
 import axios from 'axios';
 import {BACKEND_URL} from "../staticData.js"
 
-  const ActivateUser = () =>{
-
+const ActivateUser = () =>{
      const {id, token} = useParams();
      const navigate = useNavigate();
-
+  
+  //verifies mail id and activates the user
      const linkValidation = async() => 
-     {
+       { 
+          try{
+              const res = await axios.get(`${BACKEND_URL}/user/activate/${id}/${token}`);
+              console.log("res", res);
+              if(res.status==400)
+                navigate("*");
+              else
+               console.log("user activated")
+             }
+          catch(err){
+             alert(err.response.data);
+             navigate("*");
+           }
+      }
 
-       try{
-          const res = await axios.get(`${BACKEND_URL}/user/activate/${id}/${token}`);
-          console.log("res", res);
-
-          if(res.status==400)
-               navigate("*");
-
-          else
-             console.log("user activated")
-        }
-
-       catch(err){
-          alert(err.response.data);
-          navigate("*");
-        }
-     }
-
-   useEffect(()=>{
-        
+    useEffect(()=>{  
        linkValidation();
-
     }, [])
  
    return(
-
-   <>
-     <div className="container">
-        <div style={{ minHeight: "85vh", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+      <>
+       <div className="container">
+         <div style={{ minHeight: "85vh", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
           <img src="/activation-4.png" alt="Account activated" style={{ width: "500px", marginBottom: 20 }} />
-         
-          
           <NavLink to="/" className="btn btn-primary" style={{ fontSize: 18 }}> Back To Home Page </NavLink>
-        </div>
-      </div>
-
-    </>
-
+         </div>
+       </div>
+      </>
    	);
-
 } 
-
 
 export default ActivateUser;
